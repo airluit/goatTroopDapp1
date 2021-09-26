6d03f9e6d362b9dcb45786afb817d22368d06513
Ben Luitgaarden <bluitgaarden@Bens-MacBook-Pro.local>
2021-09-25 20:01:21 -0500

    init

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div className="header-wrapper">
       <nav className="nav-container">
           <a className="menu-option" href="/"> Home </a>
-          <a className="menu-option" href="about"> About </a>
+          <a className="menu-option" href="troops1/about"> About </a>
           <a className="menu-option" href="roadmap"> Roadmap </a>
           <a className="menu-option" href="about"> change </a>
       </nav>
