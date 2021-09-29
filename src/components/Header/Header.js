82a61864fe41d00b4df4907bd6194e8c5e7b7f32
Christian Louria <tian@Christians-Air.lan>
2021-09-29 19:52:21 -0400

    changed styles, nav, team

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,124 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import './HeaderStyles.css'
-import infantryLogo from '../../assets/images/troopsIfantry.png'
+import IconButton from '@mui/material/IconButton'
+import Drawer from '@mui/material/Drawer'
+import discordIcon from '../../assets/images/discord.jpg'
+import twitterIcon from '../../assets/images/twitterIcon.png'
+import openIcon from '../../assets/images/open.png'
 import infantryLogoGray from '../../assets/images/troopIfantryGray.jpeg'
+import MenuIcon from '@mui/icons-material/Menu'
 import { BrowserRouter as Router, Link, useLocation } from 'react-router-dom'
 
+const tabs = [
+  { link: '/goatTroopDapp1', text: 'Home' },
+  { link: '/goatTroopDapp1?page=buy', text: 'Buy a Goat' },
+  { link: '/goatTroopDapp1?page=map', text: 'Roadmap' },
+  { link: '/goatTroopDapp1?page=team', text: 'Team' },
+  { link: '/goatTroopDapp1?page=phase2', text: 'Story' },
+  { link: '/goatTroopDapp1?page=gallery', text: 'Gallery' },
+  { link: '/goatTroopDapp1?page=terms', text: 'Terms & Conditions' },
+  { link: '/goatTroopDapp1?page=donations', text: 'Donations' },
+]
+
+const links = [
+  { link: 'https://www.google.com', icon: discordIcon },
+  { link: 'https://www.google.com', icon: twitterIcon },
+  { link: 'https://www.google.com', icon: openIcon },
+]
+
 function Header() {
+  const size = useWindowSize()
+
+  const [isOpen, setIsOpen] = React.useState(false)
+
   return (
     <div className="header-wrapper">
-      <div className="logo"><img src={infantryLogoGray}/></div>
-      <nav className="nav-container">
-        <Link className="menu-option" to="/goatTroopDapp1">
-          Home
-        </Link>
-        <Link className="menu-option" to="/goatTroopDapp1?page=buy">
-          Buy a Goat
-        </Link>
-        <Link className="menu-option" to="/goatTroopDapp1?page=map">
-          Roadmap
-        </Link>
-        <Link className="menu-option" to="/goatTroopDapp1?page=team">
-          Team
-        </Link>
-        <Link className="menu-option" to="/goatTroopDapp1?page=phase2">
-          Story
-        </Link>
-        <Link className="menu-option" to="/goatTroopDapp1?page=gallery">
-          Gallery
-        </Link>
-        <Link className="menu-option" to="/goatTroopDapp1?page=terms">
-          {'Terms & Conditions'}
-        </Link>
-        <Link className="menu-option" to="/goatTroopDapp1?page=donations">
-          Donations
-        </Link>
-        <div className="social-icons">social icons</div>
-      </nav>
+      <div className="logo">
+        <img src={infantryLogoGray} />
+      </div>
+      {size.width > 1090 ? (
+        <nav className="nav-container">
+          {tabs.map((tab) => (
+            <Link key={tab.text} className="menu-option" to={tab.link}>
+              {tab.text}
+            </Link>
+          ))}
+          <div className="social-icons">
+            {links.map((link) => (
+              <img
+                key={link.icon}
+                src={link.icon}
+                className="header-social-icon"
+                onClick={() => {
+                  window.location.href = link.link
+                  return null
+                }}
+              />
+            ))}
+          </div>
+        </nav>
+      ) : (
+        <div className="icon-button-container ">
+          <IconButton
+            className="icon-button"
+            edge="start"
+            aria-label="menu"
+            onClick={() => setIsOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor={'right'}
+            open={isOpen}
+            onClose={() => setIsOpen(false)}
+          >
+            <div className="drawer-mui">
+              {tabs.map((tab) => (
+                <div className="drawer-item">
+                  <Link key={tab.text}  className="menu-option" to={tab.link}>
+                    {tab.text}
+                  </Link>
+                </div>
+              ))}
+              <div className="social-icons">
+                {links.map((link) => (
+                  <img
+                    key={link.icon}
+                    src={link.icon}
+                    className="header-social-icon"
+                    onClick={() => {
+                      window.location.href = link.link
+                      return null
+                    }}
+                  />
+                ))}
+              </div>
+            </div>
+          </Drawer>
+        </div>
+      )}
     </div>
   )
+}
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  })
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+  return windowSize
 }
 
 export default Header
--- src/pages/TeamPage/TeamPage.js
+++ src/pages/TeamPage/TeamPage.js
@@ -1,87 +1,138 @@
-import React, {useEffect} from 'react'
-import './TeamPageStyles.css'
+import React, { useEffect } from 'react'
 import goat1 from '../../assets/images/goatTeaser.png'
-
+import discordIcon from '../../assets/images/discord.jpg'
+import twitterIcon from '../../assets/images/twitterIcon.png'
+import './TeamPageStyles.css'
 
 function TeamPage() {
-    return (
-        <div className="">
-            <div className="header-text">Team</div>
-            <div class="gallery">
-                <a>
-                    <img src={goat1}/>
-                </a>
-                <div class="desc">
-                    <h1>
-                        Vinny Gorgeous</h1>
-                    <p>
-                        Born in the Cleveland Mafia,<br/>
-                        a retired snitch who is unable to keep
-                                              a secret decided to join the military.
-                    </p>
-                </div>
+  return (
+    <div className="">
+      <div className="header-text">Team</div>
+      <div class="gallery">
+        <a>
+          <img src={goat1} className="goat-image" />
+        </a>
+        <div class="desc">
+          <h1>Vinny Gorgeous</h1>
+          <p>
+            Born in the Cleveland Mafia,
+            <br />a retired snitch who is unable to keep a secret decided to
+            join the military.
+          </p>
+          <div className="social-container"></div>
+          <div className="social-container">
+            <img className="discord-icon" src={discordIcon} />
+            <div className="social-name">
+                Discord Name
             </div>
-
-            <div class="gallery">
-                <a>
-                    <img src={goat1}/>
-                </a>
-                <div class="desc">
-                    <h1>AirLuit</h1>
-                    <p>Busy piloting the Code Force high in the clouds,
-                        <br/>
-                        spends his days getting into dog fights<br/>
-                    </p>
-
-
-                </div>
+            <img className="discord-icon" src={twitterIcon} />
+            <div className="social-name">
+                Twitter Name
             </div>
-
-            <div class="gallery">
-                <a>
-                    <img src={goat1}/>
-                </a>
-                <div class="michelobsBox">
-                    <h1>Michelob Ultras</h1>
-                    <p>Previously an AA member,
-                        <br/>
-                        joined the military<br/>
-                        for a more tight knit group.
-                    </p>
-
-
-                </div>
-            </div>
-
-            <div class="gallery">
-                <a>
-                    <img src={goat1}/>
-                </a>
-                <div class="bulgingBox">
-                    <h1>Bulging Idiot</h1>
-                    <p>First in command in the Department of Art,<br/>
-                       showing no mercy towards sleep.
-                       
-                    </p>
-                </div>
-            </div>
-
-            <div class="gallery">
-                <a>
-                    <img src={goat1}/>
-                </a>
-                <div class="desc">
-                    <h1>NFTjoe</h1>
-                    <p>Joined the military because<br/> 
-                    he heard there was free pizza.</p>
-                    <p></p>
-
-                </div>
-            </div>
-
-
+          </div>
         </div>
-    )
+      </div>
+
+      <div class="gallery">
+        <a>
+          <img src={goat1} className="goat-image" />
+        </a>
+        <div class="desc">
+          <h1>AirLuit</h1>
+          <p>
+            Busy piloting the Code Force high in the clouds,
+            <br />
+            spends his days getting into dog fights
+            <br />
+          </p>
+          <div className="social-container">
+            <img className="discord-icon" src={discordIcon} />
+            <div className="social-name">
+                Discord Name
+            </div>
+            <img className="discord-icon" src={twitterIcon} />
+            <div className="social-name">
+                Twitter Name
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="gallery">
+        <a>
+          <img src={goat1} className="goat-image" />
+        </a>
+        <div class="michelobsBox">
+          <h1>Michelob Ultras</h1>
+          <p>
+            Previously an AA member,
+            <br />
+            joined the military
+            <br />
+            for a more tight knit group.
+          </p>
+          <div className="social-container">
+            <img className="discord-icon" src={discordIcon} />
+            <div className="social-name">
+                Discord Name
+            </div>
+            <img className="discord-icon" src={twitterIcon} />
+            <div className="social-name">
+                Twitter Name
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="gallery">
+        <a>
+          <img src={goat1} className="goat-image" />
+        </a>
+        <div class="bulgingBox">
+          <h1>Bulging Idiot</h1>
+          <p>
+            First in command in the Department of Art,
+            <br />
+            showing no mercy towards sleep.
+          </p>
+          <div className="social-container">
+            <img className="discord-icon" src={discordIcon} />
+            <div className="social-name">
+                Discord Name
+            </div>
+            <img className="discord-icon" src={twitterIcon} />
+            <div className="social-name">
+                Twitter Name
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="gallery">
+        <a>
+          <img src={goat1} className="goat-image" />
+        </a>
+        <div class="desc">
+          <h1>NFTjoe</h1>
+          <p>
+            Joined the military because
+            <br />
+            he heard there was free pizza.
+          </p>
+          <div className="social-container">
+            <img className="discord-icon" src={discordIcon} />
+            <div className="social-name">
+                Discord Name
+            </div>
+            <img className="discord-icon" src={twitterIcon} />
+            <div className="social-name">
+                Twitter Name
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default TeamPage
