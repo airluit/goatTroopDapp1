165184d43ba1549e5f15f4c75b377b63b8a10862
Ben Luitgaarden <bluitgaarden@Bens-MacBook-Pro.local>
2021-09-28 23:17:10 -0400

    updated src and css

--- src/pages/TeamPage/TeamPage.js
+++ src/pages/TeamPage/TeamPage.js
@@ -26,6 +26,7 @@ function TeamPage() {
                   <p>Piloting the Coding Force, Air Luit is often flying high in the clouds. 
                      His days are spent in dog fights leading his troops through the skies 
                      with brilliant success.</p>
+                  <p><i class="fab fa-discord"> @AirLuit</i></p>
                     
                 </div>
             </div>
@@ -49,6 +50,18 @@ function TeamPage() {
                   <h1>Bulging Idiot</h1>
                   <p>First in command of the art department,<br/>
                     General Bulging Idiot shows no mercy towards sleep </p>
+                </div>
+            </div>
+
+            <div class="gallery">
+                <a>
+                <img src={goat1}/>
+                </a>
+               <div class="desc">
+                  <h1>NFTjoe</h1>
+                  <p>Joined because we said there was free pizza.</p>
+                  <p><i class="fab fa-discord"> @NFTjoe</i></p>
+                    
                 </div>
             </div>
 
