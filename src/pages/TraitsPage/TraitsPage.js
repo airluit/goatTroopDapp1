c66e93cb6d08f29a7355274a876d57b863d5f8b3
Christian Louria <tian@Christians-Air.lan>
2021-10-03 12:08:31 -0400

    changed goat images

--- src/pages/TraitsPage/TraitsPage.js
+++ src/pages/TraitsPage/TraitsPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import AirGoat from '../../assets/images/teamGoats/air.jpeg'
-import VinGoat from '../../assets/images/teamGoats/vin.jpeg'
+import goat1 from '../../assets/images/24.jpeg'
+import goat2 from '../../assets/images/10.jpeg'
 import './TraitsPageStyles.css'
 
 const rarList = [
@@ -19,13 +19,13 @@ const rarList = [
 function TraitsPage() {
   return (
     <div className="">
-      <div className="header-text">Check doc for whats coming here</div>
+      <div className="header-text">Traits</div>
 
       <div className="traitContent">
         <div div className="imageContainerRel">
           <div className="imageContainer">
-            <img className="traitImage1" src={AirGoat} />
-            <img className="traitImage2" src={VinGoat} />
+            <img className="traitImage1" src={goat1} />
+            <img className="traitImage2" src={goat2} />
           </div>
         </div>
         <div className="traitContainer">
