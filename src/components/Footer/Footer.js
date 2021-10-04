424d6061511092b6ee9b5821c2075b5a33391a78
Ben Luitgaarden <bluitgaarden@Bens-MacBook-Pro.local>
2021-10-04 11:56:21 -0400

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,16 @@ import infantryLogoGray from '../../assets/images/troopIfantryGray.jpeg'
 import discordIcon from '../../assets/images/discord.jpg'
 import twitterIcon from '../../assets/images/twitterIcon.png'
 import openIcon from '../../assets/images/open.png'
+import calIcon from '../../assets/images/nft-calendar-transparent.png'
 import { BrowserRouter as Router, Link, useLocation } from 'react-router-dom'
 import './FooterStyles.css'
 
 const links = [
   { link: 'https://discord.gg/RXVF6gW8', icon: discordIcon },
   { link: 'https://twitter.com/GoattroopsNFT?s=20', icon: twitterIcon },
-  /*{ link: 'https://www.google.com', icon: openIcon }, */
+  { link: 'https://nftcalendar.io/event/goat-troops-infantry/', icon: calIcon}
 ]
+  /*{ link: 'https://www.google.com', icon: openIcon }, */
 
 function Footer() {
   return (
@@ -19,6 +21,7 @@ function Footer() {
       <div className="terms">Goat Troops Infantry</div>
       <div>
         <img className="infantry-icon" src={infantryLogoGray} />
+        <img className="nft-icon" src={calIcon} />
         <div className="terms-footer-wrapper">
           <div className="terms-footer">
             By purchasing a Goat Troop you will hold all commercial rights to
@@ -42,5 +45,6 @@ function Footer() {
     </div>
   )
 }
+
 
 export default Footer
